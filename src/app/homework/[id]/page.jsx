7fd8d9969af8ba8881733d13f9312e4376e90c62
Ricky Tsao <rickytsao@Rickys-MacBook-Pro.local>
2023-09-21 15:19:46 +0800

    fix url for fetching Tutorials

--- src/app/homework/[id]/page.jsx
+++ src/app/homework/[id]/page.jsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getDataById(id) {
-  console.log('Get homework from : ', `http://localhost:3000/api/posts/${id}`)
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {cache: "no-store"});
+  const res = await fetch(`api/posts/${id}`, {cache: "no-store"});
   return (!res.ok) ? notFound() : res.json();
 }
 
--- src/app/tutorial/[slug]/page.jsx
+++ src/app/tutorial/[slug]/page.jsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getDataBySlug(slug) {
-  const dataUrl = `http://localhost:3000/api/tutorials/${slug}`;
-  console.log('Get tutorial from : ', dataUrl)
+  const dataUrl = `api/tutorials/${slug}`;
   const res = await fetch(dataUrl, {cache: "no-store"});
   return (!res.ok) ? notFound() : res.json();
 }
